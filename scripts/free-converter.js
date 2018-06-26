bb7998cc560e34bca2e669af91e5705048bbd103
Jezeh Priesten <jepriesten@gmail.com>
2018-06-26 19:43:34 +0100

    Setting up free-currencyconverter API successfull

---
+++ scripts/free-converter.js
@@ -0,0 +1,28 @@
+console.log("I can convert now");
+let toConvert = document.getElementById("toConvert");
+let converted = document.getElementById("converted");
+let convert = document.getElementById("convert");
+let toSelect = document.getElementById("inputGroupSelect02");
+let fromSelect = document.getElementById("inputGroupSelect01");
+
+convert.addEventListener("click", ()=> {
+    let queryCodes = `${fromSelect.value}_${toSelect.value}`;
+    fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${queryCodes}&compact=ultra`).then(responds => {
+        return responds.json();
+    }).then(query => {
+        converted.setAttribute("value", `${toConvert.value * query[queryCodes]}`);
+        console.log(query);
+    })
+})
+
+const url = fetch(`https://free.currencyconverterapi.com/api/v5/currencies`);
+url.then(responds => {
+    return responds.json();
+}).then(data => {
+    let results = data.results
+    fromSelect.innerHTML = "" , toSelect.innerHTML = "";
+    for( const currency in results ){
+        fromSelect.innerHTML += `<option value="${results[currency].id}">${results[currency].id}</option>`;
+        toSelect.innerHTML += `<option value="${results[currency].id}">${results[currency].id}</option>`;
+    }
+});
