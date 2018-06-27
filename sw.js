1b6eb45dd338adff4d8f99c17f8515454362a2d5
Jezeh Priesten <jepriesten@gmail.com>
2018-06-27 11:34:27 +0100

    First try at fetching requests from cache

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@ let Cache_Name = 'currency-converter-v1';
 let URLsToCache = [
     '/index.html',
     'scripts/free-converter.js',
-    'https://free.currencyconverterapi.com/api/v5/currencies'
+    'https://free.currencyconverterapi.com/api/v5/currencies',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
 ];
 
 self.addEventListener("install", (event) => {
@@ -11,6 +12,21 @@ self.addEventListener("install", (event) => {
         caches.open(Cache_Name).then(cache => {
             console.log("Cache opened");
             return cache.addAll(URLsToCache); 
+        })
+    );
+}) 
+
+self.addEventListener("fetch", (event) => {
+    let requestURL = new URL(event.request.url);
+
+    if(requestURL.pathname.startsWith('/api/v5/convert')){
+        event.respondWith(conversionRates_cache(event.request));
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 })
