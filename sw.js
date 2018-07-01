0ec86c92cfacf03b5119b95e3396a623b285dd2c
Jezeh Priesten <jepriesten@gmail.com>
2018-07-01 19:27:01 +0100

    Editing the UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const Cache_Name = 'currency-converter-v5';
+const Cache_Name = 'currency-converter-v4';
 const Cache_Conversion_Rate = 'conversion-rate';
 const All_Caches = [
     Cache_Name,
@@ -21,7 +21,7 @@ self.addEventListener("install", (event) => {
         })
     );
 }); 
-  
+
 self.addEventListener("activate", event => {
     console.log("Static content added");
     event.waitUntil(
